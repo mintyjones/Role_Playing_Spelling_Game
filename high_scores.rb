7404c5fded30d17d31f641ad2dcd72f7555c75a8
mintyjones <gordon@zeedesigns.com.au>
2020-12-16 22:02:47 +1000

    :construction: sorted high score table highest score at top

--- high_scores.rb
+++ high_scores.rb
@@ -2,6 +2,9 @@ class HighScores
     def initialize
         score_array = []
         YAML.load_stream(File.read 'highscores.yml') { |doc| score_array << doc }
+        score_array.sort_by!{|x|  x[:playerscore] }.reverse!
+        # p score_array
+        # puts score_array
         @table = TTY::Table.new
         @table << ["Name","High Score"]
         score_array.each do |score|
